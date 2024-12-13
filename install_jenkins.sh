000bf4ba5a0de05a2e5e12ed4622987a7bc7da4e
Naveen kumar <62015265+naveeneee48@users.noreply.github.com>
2024-12-13 14:41:40 +0530

    Update install_jenkins.sh

--- install_jenkins.sh
+++ install_jenkins.sh
@@ -8,4 +8,10 @@ echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins
 sudo apt-get update 
 sudo apt-get install jenkins -y 
 
+sudo apt install docker.io
+
+sudo usermod -aG docker jenkins
+sudo usermod -aG docker ubuntu
+sudo systemctl restart docker
+
 sudo cat /var/lib/jenkins/secrets/initialAdminPassword
